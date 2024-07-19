bae9d0dc429f749bc25d1b0ed05e91c8ea692891
Kalpesh <kalpeshdhoke7@gmail.com>
2024-07-19 17:37:51 +0530

    changes in 3d scene

--- src/components/ThreeScene.jsx
+++ src/components/ThreeScene.jsx
@@ -7,27 +7,23 @@ import Loading from "./Loading";
 const ThreeScene = () => {
   const mountRef = useRef(null);
   const [loading, setLoading] = useState(true);
-
   useEffect(() => {
     const currentMount = mountRef.current;
-
     const scene = new THREE.Scene();
     scene.background = new THREE.Color(0x0f172a); // Set background color to #0f172a
 
     const camera = new THREE.PerspectiveCamera(75, currentMount.clientWidth / currentMount.clientHeight, 0.1, 1000);
+    camera.position.set(0, 1, 5); // Adjust camera position
     camera.position.set(0, 0, 3); // Adjust camera position
 
     const renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(currentMount.clientWidth, currentMount.clientHeight);
     currentMount.appendChild(renderer.domElement);
-
     const controls = new OrbitControls(camera, renderer.domElement);
     controls.enableDamping = true;
-
     const light = new THREE.DirectionalLight(0xffffff, 1);
-    light.position.set(100, 100, 100).normalize();
+    light.position.set(5, 5, 5).normalize();
     scene.add(light);
-
     // GLTFLoader to load a custom 3D model
     const loader = new GLTFLoader();
     loader.load(
@@ -43,24 +39,18 @@ const ThreeScene = () => {
         setLoading(false); // Set loading to false in case of error
       }
     );
-
     const animate = () => {
       requestAnimationFrame(animate);
-
       controls.update();
       renderer.render(scene, camera);
     };
-
     animate();
-
     const handleResize = () => {
       camera.aspect = currentMount.clientWidth / currentMount.clientHeight;
       camera.updateProjectionMatrix();
       renderer.setSize(currentMount.clientWidth, currentMount.clientHeight);
     };
-
     window.addEventListener('resize', handleResize);
-
     return () => {
       currentMount.removeChild(renderer.domElement);
       window.removeEventListener('resize', handleResize);
